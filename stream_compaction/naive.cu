191a0a4ec9835f8ef01ae141b424f40ef0de1272
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-09-10 22:04:37 -0400

    feat: implemented naive scan

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -11,14 +11,74 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+        __global__ void kernStep(int n, int offset, int* odata, const int* idata)
+        {
+            int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+            if (index >= n) {
+                return;
+            }
+
+
+            odata[index] = idata[index];
+            if (index >= offset)
+            {
+                odata[index] += idata[index - offset];
+            }
+        }
+
+        __global__ void kernToExclusive(int n, int* odata, const int* idata)
+        {
+            int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+            if (index >= n) return;
+
+            odata[index] = index == 0 ? 0 : idata[index - 1];
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+            
+            if (n <= 0) {
+                timer().endGpuTimer();
+                return;
+            }
+
+            int* dev_idata;
+            int* dev_odata;
+
+            cudaMalloc(&dev_idata, sizeof(int) * n);
+            cudaMalloc(&dev_odata, sizeof(int) * n);
+
+            cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+            constexpr int blockSize = 128;
+            const dim3 blocksPerGrid = (n + blockSize - 1) / blockSize;
+            const dim3 threadsPerBlock = dim3(blockSize);
+
+            const int logn = ilog2ceil(n);
+
+            for (int d = 0; d < logn; d++)
+            {
+                const int offset = 1 << d;
+
+                kernStep<<<blocksPerGrid, threadsPerBlock>>>(n, offset, dev_odata, dev_idata);
+                checkCUDAError("kernStep");
+
+                std::swap(dev_odata, dev_idata);
+            }
+
+            kernToExclusive<<<blocksPerGrid, blockSize>>>(n, dev_odata, dev_idata);
+            checkCUDAError("kernToExclusive");
+
+            cudaMemcpy(odata, dev_odata, sizeof(int) * n, cudaMemcpyDeviceToHost);
+
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
+
             timer().endGpuTimer();
         }
     }
